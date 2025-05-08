2fc25e7eb20b73ab2b1ec1f9b4e0c941d182cf03
roblesdotdev <httpsnip@gmail.com>
2025-05-08 19:58:40 -0300

    home: notes listing

--- app/src/main/java/com/roblesdotdev/jetnotes/home/presentation/list/ListScreen.kt
+++ app/src/main/java/com/roblesdotdev/jetnotes/home/presentation/list/ListScreen.kt
@@ -1,9 +1,16 @@
 package com.roblesdotdev.jetnotes.home.presentation.list
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -13,6 +20,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -35,18 +43,44 @@ fun ListScreen(viewModel: ListViewModel = hiltViewModel(), onAddNote: () -> Unit
             }
         }
     ) { innerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .padding(innerPadding)
-                .padding(16.dp)
+                .padding(horizontal = 16.dp),
+            contentPadding = PaddingValues(bottom = 24.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             if (isEmpty) {
-                EmptyListText(onClick = onAddNote)
+                item {
+                    EmptyListText(onClick = onAddNote)
+                }
+            }
+            items(viewModel.state.notes) { note ->
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surface,
+                    ),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 8.dp
+                    )
+                ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Text(note.title, style = MaterialTheme.typography.titleMedium)
+                        Text(
+                            note.description,
+                            style = MaterialTheme.typography.bodyMedium.copy(
+                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = .75f)
+                            ),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
             }
         }
     }
 }
-
 
 
 @Preview
--- app/src/main/java/com/roblesdotdev/jetnotes/home/presentation/list/ListState.kt
+++ app/src/main/java/com/roblesdotdev/jetnotes/home/presentation/list/ListState.kt
@@ -3,5 +3,9 @@ package com.roblesdotdev.jetnotes.home.presentation.list
 import com.roblesdotdev.jetnotes.home.domain.models.Note
 
 data class ListState(
-    val notes: List<Note> = emptyList()
+    val notes: List<Note> = mockNotes  // emptyList()
 )
+
+private val mockNotes = (1..30).map {
+    Note(title = "Note $it", description = "Description for note $it")
+}
